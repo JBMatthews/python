abbffb56f03c929233081fa297a9cdbb561485c6
James B. Matthews <james30152@gmail.com>
2018-07-29 18:35:08 -0400

    _add

--- py4e/assignments/11.3_assignment/_playground.py
+++ py4e/assignments/11.3_assignment/_playground.py
@@ -1,26 +1,36 @@
-# The basic outline of this problem is to read the file, look for integers using the re.findall(), looking for a regular expression of '[0-9]+' and then converting the extracted strings to integers and summing up the integers.
+# The basic outline of this problem is to read the file, look for integers using
+# the re.findall(), looking for a regular expression of '[0-9]+' and then converting
+# the extracted strings to integers and summing up the integers.
 
 # CODE BELOW:
 import re
 
+# Setting counter
+counter = 0
+
+# Just press Enter input
 fname = input("Enter file:")
 if len(fname) < 1 :
-    fname = "regex-sum-42.txt"
+    fname = "regex_sum_51632.txt"
 hand = open(fname)
 
-for line in hand:
-    strip = line.rstrip()
-    stuff = re.findall('[0-9]+', line)
-    sints = float(stuff)
-    print(type(sints))
+# Breakdown and findall
+lines = hand.read()
+num = re.findall('[0-9]+', lines)
+
+# Loop through and add them up
+for n in num:
+    count = int(n)
+    counter = counter + count
+print(counter)
 
 
-
+# #####################
+# ### --- NOTES --- ###
+# #####################
 # loop thru lines
 #
 # pull int from lines
-#
-# put in list, or dict
 #
 # convert to int
 #
