08cb6b75e50904bf3ba4104b7c3395f2b7475568
James B. Matthews <james30152@gmail.com>
2018-08-12 00:23:30 -0400

    +add

--- py4e/assignments/12.6_assignment/_play.py
+++ py4e/assignments/12.6_assignment/_play.py
@@ -1,22 +1,37 @@
-# Playground
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import ssl
+##############
+# Playground #
+##############
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+# 12.3 Retrieving an image over HTTP
+import socket
+import time
 
-url = input('Enter - ')
-html = urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, "html.parser")
+HOST = 'data.pr4e.org'
+PORT = 80
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect((HOST, PORT))
+mysock.sendall(b'GET http://data.pr4e.org/cover3.jpg HTTP/1.0\r\n\r\n')
 
-# Retrieve all of the anchor tags
-span = soup('span')
-for tag in span:
-    # Look at the parts of a tag
-    print('TAG:', tag)
-    print('URL:', tag.get('href', None))
-    print('Contents:', tag.contents[0])
-    print('Attrs:', tag.attrs)
+count = 0
+picture = b""
+
+while True:
+    data = mysock.recv(5120)
+    if (len(data) < 1): break
+    # time.sleep(0.25)
+    count = count + len(data)
+    print(len(data), count)
+    picture = picture + data
+
+mysock.close()
+
+# Look for the end of the header (2 CRLF)
+pos = picture.find(b"\r\n\r\n")
+print('Header length', pos)
+print(picture[:pos].decode())
+
+# Skip past the header and save the picture data
+picture = picture[pos+4:]
+fhand = open("stuff.jpg", "wb")
+fhand.write(picture)
+fhand.close()
