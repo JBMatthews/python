b6912be3f50a02b0ef962c84a4222426ffe83850
James B. Matthews <james30152@gmail.com>
2018-08-12 10:00:04 -0400

    +add

--- py4e/assignments/12.7_assignment/_play.py
+++ py4e/assignments/12.7_assignment/_play.py
@@ -4,6 +4,8 @@ import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
 
+count = 0
+
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -16,4 +18,12 @@ soup = BeautifulSoup(html, 'html.parser')
 # Retrieve all of the anchor tags
 tags = soup('a')
 for tag in tags:
-    print(tag.get('href', None))
+    count = count + 1
+    allhttp = tag.get('href', None)
+    for x in allhttp:
+        x = allhttp.get('.html')
+        print(x)
+
+
+# DATA
+# http://py4e-data.dr-chuck.net/known_by_Fikret.html
