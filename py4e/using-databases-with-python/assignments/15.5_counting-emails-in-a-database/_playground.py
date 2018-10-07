64efbecf64ba6b47765b6bb43e05bb25b0edc45b
James B. Matthews <james30152@gmail.com>
2018-10-07 18:52:56 -0400

    +add

--- py4e/using-databases-with-python/assignments/15.5_counting-emails-in-a-database/_playground.py
+++ py4e/using-databases-with-python/assignments/15.5_counting-emails-in-a-database/_playground.py
@@ -1,10 +1,10 @@
 import sqlite3
+import re
 
 conn = sqlite3.connect('emaildb.sqlite')
 cur = conn.cursor()
 
 cur.execute('DROP TABLE IF EXISTS Counts')
-
 cur.execute('''
 CREATE TABLE Counts (email TEXT, count INTEGER)''')
 
@@ -19,22 +19,27 @@ for line in fh:
     cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,))
     row = cur.fetchone()
     if row is None:
-        cur.execute('''INSERT INTO Counts (email, count)
-                VALUES (?, 1)''', (email,))
+        cur.execute('''INSERT INTO Counts (email, count) VALUES (?, 1)''', (email,))
     else:
-        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?',
-                    (email,))
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?', (email,))
     conn.commit()
 
 # https://www.sqlite.org/lang_select.html
 sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
 
-for row in cur.execute(sqlstr):
-    print(str(row[0]), row[1])
+cur.execute('DROP TABLE IF EXISTS Orgs')
+cur.execute('''
+CREATE TABLE Orgs (org TEXT, count INTEGER)''')
 
+for row in cur.execute(sqlstr):
+    print(row)
+    org = re.findall('@(\w+)', row[0])
+    emails = str(org)
+    # cur.execute('SELECT count FROM Orgs WHERE org = ? ', (org,))
+    # row = cur.fetchone()
+    # print(row)
+    print(emails)
 cur.close()
 
 
-#
-# for rows in cur.execute(sqlstr):
 #     orgs = re.findall('@(\w+)', rows[0])
